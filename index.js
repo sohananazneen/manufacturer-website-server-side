5ede71eb7c46229505e5678e0b244fd38145e272
SohanaN <sohananaz01@gmail.com>
2022-05-26 04:18:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fhsec.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('sn-car-parts').collection('products');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+    } finally {
+
+
+    }
+
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Car Parts')
+})
+
+app.listen(port, () => {
+    console.log(`Car Parts app listening on port ${port}`)
+})
